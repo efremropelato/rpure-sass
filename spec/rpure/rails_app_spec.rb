db7c4fcacde65645ed413e82f58781f938110bcc
Efrem Ropelato <efrem.ropelato@gmail.com>
2018-06-30 11:18:29 +0200

    rpure.css rspec

--- spec/rpure/rails_app_spec.rb
+++ spec/rpure/rails_app_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'Rails app' do
   it 'adds stylesheet files' do
-    expect(Rails.application.assets.find_asset('rpure'))
+    expect(Rails.application.assets.find_asset('rpure.css'))
       .to be_instance_of(Sprockets::Asset)
   end
 end
